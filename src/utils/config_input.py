92ae34e4873f1a35d42d42f320a98e1432bde23e
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2019-03-08 22:37:12 +0100

    changed config

---
+++ src/utils/config_input.py
@@ -0,0 +1,105 @@
+config={
+
+    "PV":{
+    "PV_1":{
+        "position":13,
+        "mqtt":{
+            "qos":1,
+            "host":"localhost",
+            "topic":"IST/PV_1"
+        }
+    },
+    "PV_2":{
+    "position":14,
+        "mqtt":{
+            "qos":1,
+            "host":"localhost",
+            "topic":"IST/PV_2"
+        }
+    },
+    "PV_3":{
+    "position":15,
+        "mqtt":{
+            "qos":1,
+            "host":"localhost",
+            "topic":"IST/PV_3"
+        }
+    },
+    "PV_4":{
+"position":16,
+        "mqtt":{
+            "qos":1,
+            "host":"localhost",
+            "topic":"IST/PV_4"
+        }
+    }},
+    "SoC":{
+    "SoC_1":{
+"position":17,
+        "mqtt":{
+            "qos":1,
+            "host":"localhost",
+            "topic":"IST/SoC_1"
+        }
+    },
+    "SoC_2":{
+"position":18,
+        "mqtt":{
+            "qos":1,
+            "host":"localhost",
+            "topic":"IST/SoC_2"
+        }
+    },
+    "SoC_3":{
+"position":19,
+        "mqtt":{
+            "qos":1,
+            "host":"localhost",
+            "topic":"IST/SoC_3"
+        }
+    },
+    "SoC_4":{
+"position":20,
+        "mqtt":{
+            "qos":1,
+            "host":"localhost",
+            "topic":"IST/SoC_4"
+        }
+    }},
+    "SW":{
+
+    "SW_1":{
+"position":56,
+        "mqtt":{
+            "qos":1,
+            "host":"localhost",
+            "topic":"IST/SW_1"
+        }
+    },
+    "SW_2":{
+"position":57,
+        "mqtt":{
+            "qos":1,
+            "host":"localhost",
+            "topic":"IST/SW_2"
+        }
+    },
+    "SW_3":{
+"position":58,
+        "mqtt":{
+            "qos":1,
+            "host":"localhost",
+            "topic":"IST/SW_2"
+        }
+    },
+    "SW_4":{
+"position":59,
+        "mqtt":{
+            "qos":1,
+            "host":"localhost",
+            "topic":"IST/SW_4"
+        }
+    }
+    }
+
+}
